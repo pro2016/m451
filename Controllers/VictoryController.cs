ec5a27aa4d0e39ca7f9031c280b1c2ebd7d5547d
pro <pro@pros-Mac-mini.local>
2016-02-28 15:45:39 +0100

    Welcome

--- Controllers/VictoryController.cs
+++ Controllers/VictoryController.cs
@@ -15,5 +15,15 @@ namespace maybe451.Controllers
         {
             return View();
         }
+        
+        public IActionResult Welcome(string name, int numTimes = 1) 
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View(); 
+        }
+        
     }
+    
+ 
 }
